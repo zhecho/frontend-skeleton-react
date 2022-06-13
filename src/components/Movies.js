564271d2e7e3931c3cd30e05465a0723603d6f29
Zhecho Zhechev <zhechev.zhecho at gmail.com>
2022-06-13 23:04:59 +0300

    feat(frontend): 6.50 Checking for errors (adding init frontend)

---
+++ src/components/Movies.js
@@ -0,0 +1,42 @@
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+
+export default class Movies extends Component {
+  state = {
+    movies: [],
+    isLoaded: false,
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:8080/v1/movies")
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          movies: json.movies,
+          isLoaded: true,
+        });
+      });
+  }
+
+  render() {
+    const { movies, isLoaded } = this.state;
+
+    if (!isLoaded) {
+      return <p>Loading...</p>;
+    } else {
+      return (
+        <Fragment>
+          <h2>Choose a movie</h2>
+
+          <ul>
+            {movies.map((m) => (
+              <li key={m.id}>
+                <Link to={`/movies/${m.id}`}>{m.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      );
+    }
+  }
+}
