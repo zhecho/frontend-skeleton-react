6e87e1b9c2b39f1c0c8201c19657032be3f7341a
Zhecho Zhechev <zhechev.zhecho at gmail.com>
2022-06-16 16:43:18 +0300

    feat(frontend): 6.50 Error handling in react

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,23 +5,38 @@ export default class Movies extends Component {
   state = {
     movies: [],
     isLoaded: false,
+    error: null,
   };
 
   componentDidMount() {
     fetch("http://localhost:8080/v1/movies")
-      .then((response) => response.json())
+      // .then((response) => response.json())
+      .then((response) => {
+        console.log("Status Code: ", response.status);
+        if (response.status !== 200) {
+            let err = new Error(response.statusText);
+            err.message = "Invalid Status Code" + response.status;
+            this.setState({ error: err });
+        }
+        return response.json()})
       .then((json) => {
         this.setState({
           movies: json.movies,
           isLoaded: true,
+        },
+        (error) => {
+        this.setState({ isLoaded: true, error });
         });
-      });
+        });
   }
 
-  render() {
-    const { movies, isLoaded } = this.state;
 
-    if (!isLoaded) {
+  render() {
+    const { movies, isLoaded, error } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
       return <p>Loading...</p>;
     } else {
       return (
